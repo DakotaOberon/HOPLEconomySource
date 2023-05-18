23aa50cd0215e851afe38cbf117e9996325af5b5
DakotaOberon <dakotaoberon@yahoo.com>
2023-05-19 02:27:22 +0700

    Remove and change Django SECRET_KEY variable
    - Removed the SECRET_KEY value from economy/settings.py and created a local .env file that holds this value.
    
    - Added the requirements.txt file

--- economy/settings.py
+++ economy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r9us*5x2g#)aqu+2x*#_mn7jg9k#h8x7fw51z%(07lsf8$**lu'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
