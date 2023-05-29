d7a453eb6693bcc255674499c1cdddf4c4a4a61c
DakotaOberon <dakotaoberon@yahoo.com>
2023-05-30 05:38:29 +0700

    Simple discord Guild model home template

--- apps/source/discord/urls.py
+++ apps/source/discord/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="discord_home"),
+    path(r'guild/<int:guild_id>/', views.guild_home, name='discord_guild_home')
 ]
--- apps/source/discord/views.py
+++ apps/source/discord/views.py
@@ -6,3 +6,19 @@ def home(request):
     guilds_list = Guild.objects.order_by("name")
     clients_list = Client.objects.order_by("name")
     return render(request, "discord_home.html", {"guilds_list": guilds_list, "clients_list": clients_list})
+
+def guild_home(request, guild_id):
+    try:
+        guild = Guild.objects.get(id=guild_id)
+    except Guild.DoesNotExist:
+        guild = None
+    
+    guild_info = {}
+    if (guild):
+        guild_info = {
+            "id": guild.id,
+            "name": guild.name,
+            "total_members": len(guild.members.all()),
+        }
+
+    return render(request, "discord_guild_home.html", guild_info)
